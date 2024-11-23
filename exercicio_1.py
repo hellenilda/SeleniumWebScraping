49d4f7efc8059e23eaa53ee206932a27e7f35cec
Hellen Lima Araujo <202415020015@ad.ifpb.edu.br>
2024-11-23 10:00:49 -0300

    :sparkles: feat: confirmação de fim de código + comentários

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Firefox()
-driver.get('https://google.com')
-search = driver.find_element("name", 'q')
-search.send_keys("Python Testing with Selenium")
-search.submit()
+driver = webdriver.Firefox()        # Navegador
+driver.get('https://google.com')    # Recuperando a URL
+search = driver.find_element("name", 'q') # Acessando a barra de pesquisa
+search.send_keys("Python Testing with Selenium")    # Valor na barra de pesquisa
+search.submit()     # Enviar o valor/texto de pesquisa
 
-sleep(5)
-driver.quit()
+sleep(5)        # Tempo de espera
+driver.quit()   # Fechando o navegador
+print("Navegador finalizado")
